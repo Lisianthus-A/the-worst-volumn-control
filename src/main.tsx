37a49ceab7c53f84fee1e66eaf7ffe38f94c30af
Lisianthus-A <lisianthus.zeng@gmail.com>
2025-01-22 01:29:43 +0800

    fix(Router): wrap App component with Router for improved routing handling

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { Router } from "wouter";
 import store from "@/store";
 import "./global.css";
 import App from "./App";
 
+const base = import.meta.env.DEV ? "/" : "/the-worst-volumn-control";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <Router base={base}>
+        <App />
+      </Router>
     </Provider>
   </StrictMode>
 );
