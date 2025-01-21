05f1faaf3678399dfff3fa4ad5c20586737d87ce
Lisianthus-A <Lisianthus-A@users.noreply.github.com>
2025-01-21 17:19:17 +0000

    Automated deployment: Tue Jan 21 17:19:17 UTC 2025 5a418097da790b39a5e4f11706486e90af192cc9

---
+++ assets/index-CCrdq_xM.js
@@ -0,0 +1,1 @@
+import{j as a,a as t}from"./index-YQgQLy7v.js";function e(){const n=()=>{t.setVolume(Math.random()*100>>0)};return a.jsx("button",{onClick:n,children:"Change"})}export{e as default};
