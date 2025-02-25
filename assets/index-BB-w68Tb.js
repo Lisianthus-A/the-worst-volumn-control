3a4fb0b40f393982cb1127cfc25c34b22cddbd0e
Lisianthus-A <Lisianthus-A@users.noreply.github.com>
2025-02-25 07:34:29 +0000

    Automated deployment: Tue Feb 25 07:34:29 UTC 2025 5f6fe61e272220bec61f99410026ea1ee1e1064b

--- assets/index-BB-w68Tb.js
+++
@@ -1,1 +0,0 @@
-import{j as a,a as t}from"./index-Cn3vPFBM.js";function e(){const n=()=>{t.setVolume(Math.random()*100>>0)};return a.jsx("button",{onClick:n,children:"Change"})}export{e as default};
