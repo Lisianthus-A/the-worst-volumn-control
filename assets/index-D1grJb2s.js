4131eff1b4e1b2b925356bf65e5e21aa2a01efbb
Lisianthus-A <Lisianthus-A@users.noreply.github.com>
2025-03-15 05:19:47 +0000

    Automated deployment: Sat Mar 15 05:19:47 UTC 2025 31f4cde4dbece39b68e05915554f1f247a91c432

---
+++ assets/index-D1grJb2s.js
@@ -0,0 +1,1 @@
+import{j as a,a as t}from"./index-Cq7dXzu2.js";function e(){const n=()=>{t.setVolume(Math.random()*100>>0)};return a.jsx("button",{onClick:n,children:"Change"})}export{e as default};
