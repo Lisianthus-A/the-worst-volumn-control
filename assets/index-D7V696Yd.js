e99541c5ba740ae49afa05a73a31470341b6bda3
Lisianthus-A <Lisianthus-A@users.noreply.github.com>
2025-03-24 10:22:29 +0000

    Automated deployment: Mon Mar 24 10:22:29 UTC 2025 35464a556025c734e17785005ead833d4712d5f6

---
+++ assets/index-D7V696Yd.js
@@ -0,0 +1,1 @@
+import{j as a,a as t}from"./index-DhQMT8-O.js";function e(){const n=()=>{t.setVolume(Math.random()*100>>0)};return a.jsx("button",{onClick:n,children:"Change"})}export{e as default};
